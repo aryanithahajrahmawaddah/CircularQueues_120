29fb3e9fe503b6e6ad69c6de3edec0ecafe36c4e
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-06-22 15:54:49 +1200

    menampilkan beberapa pilihan saat menjalankan program

--- CircularQueues_120/CircularQueues_120.cpp
+++ CircularQueues_120/CircularQueues_120.cpp
@@ -99,3 +99,21 @@ public:
 	}
 };
 
+int main() {
+	Queues q;
+	char ch;
+
+	while (true) {
+		try{
+			cout << "Menu" << endl;
+			cout << "1. Implemen insert operation" << endl;
+			cout << "2. Implment deleted operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4)" << endl;
+			cin >> ch;
+			cout << endl;
+
+
+			
+}
